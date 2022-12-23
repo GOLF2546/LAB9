286b14b187abd14285dd326382e1a95132d639bc
PATTARAPOL NA NAKORN <pattarapol_nanakorn@cmu.ac.th>
2022-12-23 15:51:22 +0700

    Finish Lab9_3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,46 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        int height;
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100){
+            character = "Chopper";
+        }else if(height < 180){
+            character = "Usopp";
+        }else{
+            long value;
+            cout << "Enter your bounty: ";
+            cin >> value;
+            if(value > 1100000000){
+                character = "Zoro";
+            }else{
+                character = "Sanji";
+            }
+        }
+    } else{
+        if(age < 61){
+            long value;
+            cout << "Enter your bounty: ";
+            cin >> value;
+            if(value > 500000000){
+                character = "Jinbe";
+            }else{
+                character = "Franky";
+            }
+        }else{
+            character = "Brook"; 
+        }
+    }
+    cout << "Your character = "<< character;
+}
